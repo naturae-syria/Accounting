9aad40f494f1a9c326baf368efde04a809911bd6
naturae-syria <amjadalmouayad@gmail.com>
2025-06-11 13:29:34 -0300

    Use cookie for AuthCheck

--- test/api-auth-check.test.ts
+++ test/api-auth-check.test.ts
@@ -1,12 +1,19 @@
 import { strict as assert } from 'assert'
 import { GET } from '../app/api/auth/check/route'
 
-const reqNoCookie = new Request('http://localhost')
-const resNoCookie = await GET(reqNoCookie)
-const dataNoCookie = await resNoCookie.json()
-assert.equal(dataNoCookie.valid, false)
+async function run() {
+  const reqNoCookie = new Request('http://localhost')
+  const resNoCookie = await GET(reqNoCookie)
+  const dataNoCookie = await resNoCookie.json()
+  assert.equal(dataNoCookie.valid, false)
 
-const reqCookie = new Request('http://localhost', { headers: { cookie: 'session=auth' } })
-const resCookie = await GET(reqCookie)
-const dataCookie = await resCookie.json()
-assert.equal(dataCookie.valid, true)
+  const reqCookie = new Request('http://localhost', { headers: { cookie: 'session=auth' } })
+  const resCookie = await GET(reqCookie)
+  const dataCookie = await resCookie.json()
+  assert.equal(dataCookie.valid, true)
+}
+
+run().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
