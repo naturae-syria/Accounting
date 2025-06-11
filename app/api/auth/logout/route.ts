8bf984ebed42df636e09665cf61df21622e194bf
naturae-syria <amjadalmouayad@gmail.com>
2025-06-11 11:25:38 -0300

    feat(auth): set secure cookie

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,6 +3,15 @@ import { serialize } from 'cookie'
 
 export async function POST() {
   const headers = new Headers()
-  headers.append('Set-Cookie', serialize('session', '', { httpOnly: true, path: '/', expires: new Date(0) }))
+  headers.append(
+    'Set-Cookie',
+    serialize('session', '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      maxAge: 0,
+      path: '/',
+    })
+  )
   return new NextResponse(null, { headers })
 }
