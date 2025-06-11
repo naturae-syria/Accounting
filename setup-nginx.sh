ca279030e12e92fb565453166167079036306a66
naturae-syria <amjadalmouayad@gmail.com>
2025-06-11 18:12:02 -0300

    Proxy Grafana through Nginx

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -45,6 +45,19 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
+
+    # Proxy Grafana dashboard
+    location /grafana/ {
+        proxy_pass http://localhost:4000/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
     
     # تكوين ضغط Gzip
     gzip on;
